1b6d15191a19918314f298407ceb7a6dc89a5296
Md Shimul Molla <shimuliithost@gmail.com>
2025-10-06 18:14:09 +0600

    About page desing complated

--- src/components/modules/Home/Home.tsx
+++ src/components/modules/Home/Home.tsx
@@ -3,11 +3,13 @@ import Hero from './Banner/Hero'
 import LatestBlog from './LatestBlog/LatestBlog'
 import FAQ from './FAQ/FAQ'
 import Project from './Project/Project'
+import About from './About/About'
 
 const Home = () => {
   return (
     <div>
       <Hero></Hero>
+      <About></About>
       <Project></Project>
       <LatestBlog></LatestBlog>
       <FAQ></FAQ>
