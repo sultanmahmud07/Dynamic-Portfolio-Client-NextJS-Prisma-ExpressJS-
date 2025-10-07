bde769a7270f48a025d8e77b07c7c2823010ffcd
Md Shimul Molla <shimuliithost@gmail.com>
2025-10-07 15:30:18 +0600

    Solved build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/providers/AuthProvider";
-import { Toaster } from "sonner";
+import  { Toaster } from 'react-hot-toast';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          <Toaster richColors position="top-center" />
+          <Toaster />
           {children}
         </AuthProvider>
       </body>
