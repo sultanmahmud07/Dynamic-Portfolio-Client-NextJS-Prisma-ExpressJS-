fed619574c2e62d74fe75e336f746fbd6aed7f24
sultanmahmud07 <asadkhan01862@gmail.com>
2025-10-14 15:18:23 +0600

    Added new middleware for authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,34 @@
-export { default } from "next-auth/middleware";
+import { NextRequest, NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+ 
+// 1. Specify protected and public routes
+const protectedRoutes = ['/dashboard', '/profile', '/settings']
+ 
+export default async function middleware(req: NextRequest) {
+  // 2. Check if the current route is protected or public
+  const path = req.nextUrl.pathname
+  const isProtectedRoute = protectedRoutes.includes(path)
+ 
+  // 3. Decrypt the session from the cookie
+  const cookie = (await cookies()).get('accessToken')?.value
 
-export const config = { matcher: ["/dashboard"] };
+  // 4. Redirect to /login if the user is not authenticated
+  if (isProtectedRoute && !cookie) {
+    return NextResponse.redirect(new URL('/login', req.nextUrl))
+  }
+ 
+  // 5. Redirect to /dashboard if the user is authenticated
+  if (
+    cookie &&
+    !req.nextUrl.pathname.startsWith('/dashboard')
+  ) {
+    return NextResponse.redirect(new URL('/dashboard', req.nextUrl))
+  }
+ 
+  return NextResponse.next()
+}
+ 
+// Routes Middleware will be applied to
+export const config = {
+  matcher: ['/dashboard/:path*', '/profile/:path*', '/login'],
+}
